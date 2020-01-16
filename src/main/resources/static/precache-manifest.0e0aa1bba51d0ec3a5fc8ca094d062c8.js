ae18394acebde2f77be407a39cf9c9318b673a2d
drazzazmaj <drax71@gmail.com>
2020-01-16 19:11:35 +0100

    react

---
+++ src/main/resources/static/precache-manifest.0e0aa1bba51d0ec3a5fc8ca094d062c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "543186e8b2db6bf494be9c0d3a66fe16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c9d44b5d5428ddf4601",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "560a7408bedc05225de7",
+    "url": "/static/css/main.abdc95a3.chunk.css"
+  },
+  {
+    "revision": "0c9d44b5d5428ddf4601",
+    "url": "/static/js/2.9748e76c.chunk.js"
+  },
+  {
+    "revision": "560a7408bedc05225de7",
+    "url": "/static/js/main.0d120f46.chunk.js"
+  },
+  {
+    "revision": "efc2e0a6ac4660bb2763",
+    "url": "/static/js/runtime-main.ace3b8a9.js"
+  }
+]);
